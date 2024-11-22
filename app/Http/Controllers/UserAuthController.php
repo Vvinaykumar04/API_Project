919729e3635356c498275937521a004fb7ec08c3
Vvinaykumar04 <vinaykgupta04@gmail.com>
2024-11-22 10:51:05 +0530

    Signup api change

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserAuthController extends Controller
 {
     public function login(Request $request){
-        return "Login";
+        return "Login function";
     }
 
     public function signup(Request $request){
-        return "Signup";
+        $input = $request->all();
+        $input["password"] = bcrypt($input["password"]);
+        $user = User::create($input);
+        $success["token"] = $user->createToken("MyApp")->plainTextToken;
+        $user["name"] = $user->name;
+        return["success"=>true,"resule"=>$success,"msg"=>"User register successfully"];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserAuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::post("signup",[UserAuthController::class,'signup']);
+Route::post("login",[UserAuthController::class,'login']);

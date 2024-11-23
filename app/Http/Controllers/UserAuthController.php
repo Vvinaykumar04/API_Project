645003cedb6cfa3c9a457836622cb310b18a992f
Vvinaykumar04 <vinaykgupta04@gmail.com>
2024-11-23 11:57:28 +0530

    Api login change

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserAuthController extends Controller
 {
     public function login(Request $request){
-        return "Login function";
+        $user = User::where("email",$request->email)->first();
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return ["result"=>"User not found","Success"=>false]; 
+        }else{
+            $success["token"] = $user->createToken("MyApp")->plainTextToken;
+            $user["name"] = $user->name;
+            return["success"=>true,"result"=>$success,"msg"=>"User login successfully"];
+        }
     }
 
     public function signup(Request $request){
@@ -17,6 +25,6 @@ class UserAuthController extends Controller
         $user = User::create($input);
         $success["token"] = $user->createToken("MyApp")->plainTextToken;
         $user["name"] = $user->name;
-        return["success"=>true,"resule"=>$success,"msg"=>"User register successfully"];
+        return["success"=>true,"result"=>$success,"msg"=>"User register successfully"];
     }
 }
